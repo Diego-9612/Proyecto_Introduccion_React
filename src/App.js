3327b16787305bd2a58fad0de77055c2f809339e
Diego Fernando Guerrero Perez <diego.guerrero9612@gmail.com>
2025-03-01 16:14:35 -0500

    se vuelve a crear ek proyecto sin fallas desde  cero

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import { TodoTitle } from './TodoTitle';
-import { TodoSearch } from './TodoSearch';
-import { TodoCreateButton } from './TodoCreateButton';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import React from 'react';
-
-const defaultTodos = [
-  { Text: "Tomar el curso de react", completd: true },
-  { Text: "Tomar el curso de spring", completd: false },
-  { Text: "Tomar el curso de mysql", completd: false },
-  { Text: "Tomar el curso de mysql", completd: false }];
+import logo from './platzi.webp';
+import './App.css';
 
 function App() {
   return (
-    <React.Fragment>
-
-      < TodoTitle completed={16} total={49} />
-      < TodoSearch />
-
-      < TodoList>
-        {defaultTodos.map(todo => (
-          <TodoItem key={todo.Text} Text={todo.Text}/>
-        ))}
-      </TodoList>
-
-      <TodoCreateButton />
-    </React.Fragment>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edita el archivo <code>src/App.js</code> y guarda para recargar.
+        </p>
+        <a
+          className="App-link"
+          href="https://platzi.com/reactjs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/TodoSearch.js
+++
@@ -1,9 +0,0 @@
-import './TodoSearch.css'
-function TodoSearch(){
-    return(
-        <input placeholder="Estudiar Programacion con React"
-        className='TodoSearch'/>
-    );
-}
-
-export {TodoSearch};
